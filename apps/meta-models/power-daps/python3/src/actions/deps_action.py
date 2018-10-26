16dcb39df44e7885bc035e3b9953d31a2f82c36b
Prasanna Pendse <prasanna.pendse@gmail.com>
2018-10-26 21:04:14 +0530

    Modified deps_action in python to run dependencies. Lot of work needs to be done with this still. Need to move it outside the meta model. Need to make it work per stage. Need to have appropriate options for each installer. Need to support version numbers

--- apps/meta-models/power-daps/python3/src/actions/deps_action.py
+++ apps/meta-models/power-daps/python3/src/actions/deps_action.py
@@ -1,18 +1,38 @@
 import os
 import common
 import glob
+import dependencies
 
 class DepsAction():
   name = "deps"
+  default_dapfile_location = "Dapfile"
 
-  def __init__(self):
+  def __init__(self, dapfile_location = ""):
+    self.set_dapfile_location(dapfile_location)
     return
  
   def run(self):
-    common.stop_if_failed(*common.run_command(['/usr/local/bin/pip3', '-q', 'install', 'pyinstaller']))
-    common.stop_if_failed(*common.run_command(['/usr/local/bin/pip3', '-q', 'install', 'pyparsing']))
-    common.stop_if_failed(*common.run_command(['/usr/local/bin/pip3', '-q', 'install', 'pyyaml']))
+    deps = dict()
+    with open(self.dapfile_location) as f:
+      dapfile_contents = f.read()
+      deps = dependencies.Dependencies(dapfile_contents)
+    f.closed
+
+    for dep in deps.dependencies_for("default"):
+      common.stop_if_failed(*common.run_command(self.install_command(dep)))
+
     return 0, ""
+
+  def set_dapfile_location(self, dapfile_location):
+    if dapfile_location:
+      self.dapfile_location = dapfile_location
+    else:
+      self.dapfile_location = DepsAction.default_dapfile_location
+
+  def install_command(self, dependency):
+    installers = dict()
+    installers["pip3"] = '/usr/local/bin/pip3'
+    return [installers[dependency.installer], '-q', 'install', dependency.name]
 
 def action():
    return DepsAction()
