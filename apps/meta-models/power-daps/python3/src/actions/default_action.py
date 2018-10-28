60f4753ad7fb12876e4341a5af3457f4e7409d09
Prasanna Pendse <prasanna.pendse@gmail.com>
2018-10-28 15:48:08 +0530

    remove some unused code

--- apps/meta-models/power-daps/python3/src/actions/default_action.py
+++ apps/meta-models/power-daps/python3/src/actions/default_action.py
@@ -37,24 +37,4 @@ class DefaultAction():
 def action():
    return DefaultAction()
 
-def print_actions():
-    actions = dict()
-    f = open("/Users/ppendse/src/power-daps/apps/dap/config/actions.csv", "r")
-    try:
-        raw_actions = f.read().split("\n")
-        raw_actions.remove("ACTION,TYPE,DEPENDENCY,VERSION")
-        for raw_action in raw_actions:
-            if len(raw_action.split(",")) == 4:
-                (action, type, dependency, version) = tuple(raw_action.split(","))
-                if action in actions:
-                    actions[action].append([type, dependency, version])
-                else:
-                    actions[action] = list()
-                    actions[action].append([type, dependency, version])
-        for action in iter(actions):
-            print_raw(" --- " + action)
-            for dep in iter(actions[action]):
-                print_raw("      --- " + dep[0] + ", " + dep[1])
-    finally:
-        f.close()
 
