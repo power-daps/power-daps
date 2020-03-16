b576dc0935c1d7cb6d3c79865b3cd867aa79115c
Prasanna Pendse <prasanna.pendse@gmail.com>
2020-03-16 23:14:09 +0530

    Got the template to work with intellij

--- apps/meta-models/power-daps/java9/src/actions/init_action.py
+++ apps/meta-models/power-daps/java9/src/actions/init_action.py
@@ -1,5 +1,6 @@
 import common
 import glob
+import os, pathlib
 
 from meta_model import MetaModel
 
@@ -8,11 +9,33 @@ class InitAction():
 
   def run(self):
     common.print_verbose("Running " + self.name + " action")
+    project_dir = '.'
+    project_name = os.getcwd().split('/')[-1]
 
-    files_to_copy = glob.glob(MetaModel(common.meta_model()).template_for_action(self.name) + "/*", recursive=False)
-    command_to_run = ['/bin/cp', "-R", *files_to_copy, '.']
-
+    files_to_copy = [str(p) for p in pathlib.Path(MetaModel(common.meta_model()).template_for_action(self.name)).glob("*")]
+    # files_to_copy = glob.glob(MetaModel(common.meta_model()).template_for_action(self.name) + "/*", recursive=False)
+    command_to_run = ['/bin/cp', "-R", *files_to_copy, project_dir]
     common.run_command(command_to_run)
+
+    files_to_rename = [str(p) for p in pathlib.Path(project_dir).glob("*PROJECT_NAME*")]
+    for file_to_rename in files_to_rename:
+      rename_command = ['/bin/mv', file_to_rename, file_to_rename.replace("PROJECT_NAME", project_name)]
+      common.run_command(rename_command)
+
+    files_to_search_and_replace_within = [str(p) for p in pathlib.Path(project_dir).glob(".idea/*.xml")]
+    for f in files_to_search_and_replace_within:
+        sed_command = ['/usr/bin/sed', '-ie', "s/PROJECT_NAME/" + project_name + "/g", f]
+        common.run_command(sed_command)
+
+    git_init_command = ['/usr/bin/git', 'init']
+    common.run_command(git_init_command)
+
+    git_add_command = ['/usr/bin/git', 'add', '.']
+    common.run_command(git_add_command)
+
+    git_commit_command = ['/usr/bin/git', 'commit', '-m', 'Initial commit']
+    common.run_command(git_commit_command)
+
     common.print_raw("Initialized new Java 9 application.")
 
     return 0, ""
