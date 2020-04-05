c41986b8d6f4dbc99c43b9b58db4762036de38c2
Prasanna Pendse <prasanna.pendse@gmail.com>
2020-04-05 15:43:33 +0530

    Extracted Java compilation steps into JavacCommand

--- apps/meta_models/power_daps/java9/src/power_daps/java9/actions/compile_action.py
+++ apps/meta_models/power_daps/java9/src/power_daps/java9/actions/compile_action.py
@@ -15,51 +15,22 @@
 #  You should have received a copy of the GNU General Public License
 #  along with power-daps.  If not, see <https://www.gnu.org/licenses/>.
 
-from shutil import which
-from dap_core import common
+from dap_core.base_actions.dap_action_base import DapActionBase
+from power_daps.java9.javac_command import JavacCommand
 
 
-class CompileAction:
-  name = "compile"
+class CompileAction(DapActionBase):
 
-  def __init__(self, source_dir="src", target_dir="target/production", classpath=""):
+  def __init__(self, source_dir="src", target_dir="target/production", classpath="."):
+    super().__init__()
     self.source_dir = source_dir
     self.target_dir = target_dir
     self.classpath = classpath
 
   def run(self):
-    common.print_info("Running " + self.name + " action")
-
-    cp_string = ""
-    if self.classpath != "":
-      cp_string = " -cp " + self.classpath
-
-    common.run_command_in_shell('rm -rf ' + self.target_dir)
-    common.run_command_in_shell('mkdir -p ' + self.target_dir)
-
-    common.run_command_in_shell('rm -rf ' + self.target_dir)
-    common.run_command_in_shell('mkdir -p ' + self.target_dir)
-    exit_code = 0
-    output = ""
-    if self.javac_is_installed():
-      exit_code, output = common.run_command_in_shell('find ' + self.source_dir + \
-                                  ' -type f -name "*.java" -print | xargs ' + which('javac') + ' ' + \
-                                  cp_string + " " + \
-                                  ' -d ' + self.target_dir + ' -sourcepath ' + self.source_dir)
-      try:
-        output = output.decode()
-      except (UnicodeDecodeError, AttributeError):
-        pass
-    else:
-      exit_code = 1
-      output = "'javac' not found in PATH. Is JDK installed?"
-
-    return exit_code, output
-
-  def javac_is_installed(self):
-    if which('javac'):
-      return True
-    return False
+    super().run()
+    JavacCommand(self.source_dir, self.target_dir, self.classpath).run()
+    return 0, ""
 
 
 def action():
--- apps/meta_models/power_daps/java9/src/power_daps/java9/actions/compile_test_action.py
+++ apps/meta_models/power_daps/java9/src/power_daps/java9/actions/compile_test_action.py
@@ -15,53 +15,22 @@
 #  You should have received a copy of the GNU General Public License
 #  along with power-daps.  If not, see <https://www.gnu.org/licenses/>.
 
-from shutil import which
-from dap_core import common
+from dap_core.base_actions.dap_action_base import DapActionBase
+from power_daps.java9.javac_command import JavacCommand
 
 
-class CompileTestAction:
-  name = "compile_test"
+class CompileTestAction(DapActionBase):
 
   def __init__(self, source_dir="test", target_dir="target/test", classpath=".:target/production"):
+    super().__init__()
     self.source_dir = source_dir
     self.target_dir = target_dir
     self.classpath = classpath
 
   def run(self):
-    common.print_info("Running " + self.name + " action")
-    cp_string = ""
-    if self.classpath != "":
-      cp_string = " -cp " + self.classpath + ":" + self.libs_classpath()
-    else:
-      cp_string = " -cp " + self.libs_classpath()
-
-    common.run_command_in_shell('rm -rf ' + self.target_dir)
-    common.run_command_in_shell('mkdir -p ' + self.target_dir)
-    exit_code = 0
-    output = ""
-    if self.javac_is_installed():
-      exit_code, output = common.run_command_in_shell('find ' + self.source_dir + \
-                                ' -type f -name "*.java" -print | xargs ' + which('javac') + ' ' + \
-                                cp_string + " " + \
-                                ' -d ' + self.target_dir + ' -sourcepath ' + self.source_dir)
-      try:
-        output = output.decode()
-      except (UnicodeDecodeError, AttributeError):
-        pass
-    else:
-      exit_code = 1
-      output = "'javac' not found in PATH. Is JDK installed?"
-
-    return exit_code, output
-
-  def libs_classpath(self):
-    libs = common.run_command_in_shell('find lib/java -type f -name "*.jar" -print')[1]
-    return ":".join(libs.splitlines())
-
-  def javac_is_installed(self):
-    if which('javac'):
-      return True
-    return False
+    super().run()
+    JavacCommand(self.source_dir, self.target_dir, self.classpath).run()
+    return 0, ""
 
 def action():
   return CompileTestAction()
