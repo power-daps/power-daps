a1ed127e89a428d1d9ea31bb47502fa4a8f7d8f5
Prasanna Pendse <prasanna.pendse@gmail.com>
2020-04-05 14:35:01 +0530

    Python meta model uses default_action_base too

--- apps/meta_models/power_daps/python3/src/power_daps/python3/actions/default_action.py
+++ apps/meta_models/power_daps/python3/src/power_daps/python3/actions/default_action.py
@@ -15,46 +15,17 @@
 #  You should have received a copy of the GNU General Public License
 #  along with power-daps.  If not, see <https://www.gnu.org/licenses/>.
 
-from dap_core import common
-import yaml
-from . import deps_action, unit_test_action, package_action
+from dap_core.base_actions.default_action_base import DefaultActionBase
 
 
-class DefaultAction:
-  name = "default"
-  default_actions_file_location = common.actions_file_location()
-
-  def __init__(self, actions_file_location = ""):
-    self.set_actions_file_location(actions_file_location)
-    return
+class DefaultAction(DefaultActionBase):
+  def __init__(self, actions_file_location=""):
+    super().__init__(actions_file_location)
 
   def run(self):
-    common.print_info("Running " + self.name + " action")
-
-    with open(self.actions_file_location) as f:
-      actions_file_contents = f.read()
-      for stage in yaml.load(actions_file_contents, Loader=yaml.SafeLoader).items():
-        for an_action in stage[1]:
-          common.stop_if_failed(*self.action_for(an_action).run())
-    f.closed
+    super().run()
     return 0, ""
-
-  def action_for(self, action_name):
-    actions = dict()
-    actions["deps"] = deps_action.action()
-    actions["unit_test"] = unit_test_action.action()
-    actions["package"] = package_action.action()
-
-    return actions[action_name]
-
-  def set_actions_file_location(self, actions_file_location):
-    if actions_file_location:
-      self.actions_file_location = actions_file_location
-    else:
-      self.actions_file_location = DefaultAction.default_actions_file_location
 
 
 def action():
-   return DefaultAction()
-
-
+  return DefaultAction()
