6da876b62bcaf4be7c4d4c346e29e32b5ede9853
Prasanna Pendse <prasanna.pendse@gmail.com>
2020-04-05 10:25:37 +0530

    Started extracting things needed to handle templates for init actions

--- apps/dap_core/src/dap_core/str_util.py
+++ apps/dap_core/src/dap_core/str_util.py
@@ -21,3 +21,8 @@ import re
 def camel_to_snake(name):
   name = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', name)
   return re.sub('([a-z0-9])([A-Z])', r'\1_\2', name).lower()
+
+
+def camelize(s):
+  # return s.replace("_", " ").title().replace(" ", '')
+  return ''.join(word.title() for word in s.split('_'))
--- apps/meta_models/power_daps/rust/src/power_daps/rust/actions/init_action.py
+++ apps/meta_models/power_daps/rust/src/power_daps/rust/actions/init_action.py
@@ -15,7 +15,7 @@
 #  You should have received a copy of the GNU General Public License
 #  along with power-daps.  If not, see <https://www.gnu.org/licenses/>.
 
-from dap_core import common
+from dap_core import common, str_util, template_util
 import os, sys, pathlib, shutil
 from dap_core.meta_model import MetaModel
 from dap_core.dap_action import DapAction
@@ -32,68 +32,16 @@ class InitAction(DapAction):
     project_dir = '.'
     project_name = os.getcwd().split('/')[-1]
 
-    self.check_that_name_does_not_have_dashes(project_name)
+    template_util.check_that_name_does_not_have_dashes(project_name)
     exit_code, output = CargoCommand('init').run()
-    self.copy_template_files_to(project_dir)
-    self.rename_files(project_dir, "PROJECT_NAME", project_name)
-    self.rename_files(project_dir, "PROJECT_CAMELIZED_NAME", self.camelize(project_name))
-    self.add_to_git(project_dir)
+    template_util.copy_template_files_to(project_dir, common.action_name(self))
+    template_util.find_and_replace_in_file_names_and_content(project_dir, {
+      "PROJECT_NAME": project_name,
+      "PROJECT_CAMELIZED_NAME": str_util.camelize(project_name)})
+    template_util.add_to_git(project_dir)
     common.print_raw("Initialized new Rust application.")
 
     return 0, ""
-
-  def copy_template_files_to(self, destination):
-    common.print_verbose("Looking for files to copy in: " + str(pathlib.Path(MetaModel(common.meta_model()).template_for_action(common.action_name(self)))))
-    files_to_copy = [str(p) for p in pathlib.Path(MetaModel(common.meta_model()).template_for_action(common.action_name(self))).glob("*")]
-    common.print_verbose("Found " + str(len(files_to_copy)) + " files to copy.")
-    command_to_run = ['/bin/cp', "-R", *files_to_copy, destination]
-    common.run_command(command_to_run)
-
-  def rename_files(self, dir, str_to_find, str_to_replace_with):
-    dirs = sorted(common.dirs_in(dir, ["__pycache__", "dist", "build", "egg-info", ".git"]) + ["."], key=len, reverse=True)
-    for d in dirs:
-      files_to_rename = [str(p) for p in pathlib.Path(d).glob("*" + str_to_find + "*")]
-      for f in files_to_rename:
-        common.print_verbose("Renaming " + f + " to " + f.replace(str_to_find, str_to_replace_with))
-        rename_command = ['/bin/mv', f, f.replace(str_to_find, str_to_replace_with)]
-        common.run_command(rename_command)
-
-      if str_to_find in str(d):
-        common.print_verbose("Renaming " + d + " to " + d.replace(str_to_find, str_to_replace_with))
-        rename_command = ['/bin/mv', d, d.replace(str_to_find, str_to_replace_with)]
-        common.run_command(rename_command)
-
-    grep_files_command = [shutil.which('find'), ".", "!", "-name", '*.pyc', "!", "-path", '*.git*', "-type", "f", "-exec", shutil.which("grep"), "-l", "PROJECT_NAME", '{}', ";", "-print"]
-    files_to_search_and_replace_within = common.run_command(grep_files_command)[1].splitlines()
-
-    for f in files_to_search_and_replace_within:
-      sed_command = self.sed_find_and_replace_command(str_to_find, str_to_replace_with, f)
-      common.run_command(sed_command)
-
-  def sed_find_and_replace_command(self, str_to_find, str_to_replace_with, filename):
-    sed_command = [shutil.which('sed'), '-i']
-    if sys.platform.startswith('darwin'):
-      sed_command += [""]
-    sed_command += ['-e', "s/" + str_to_find + "/" + str_to_replace_with + "/g", filename]
-    return sed_command
-
-  def add_to_git(self, dir):
-    os.chdir(dir)
-
-    git_add_command = ['/usr/bin/git', 'add', '.']
-    common.run_command(git_add_command)
-
-    git_commit_command = ['/usr/bin/git', 'commit', '-m', 'Initialized with power_daps template power_daps/rust']
-    common.run_command(git_commit_command)
-
-    return 0, ""
-
-  def camelize(self, s):
-    return s.replace("_", " ").title().replace(" ", '')
-
-  def check_that_name_does_not_have_dashes(self, name):
-    if "-" in name:
-      common.exit_with_error_message("Name " + name + " has dashes. Please use underscores as dashes cause problems in the python ecosystem")
 
 
 def action():
